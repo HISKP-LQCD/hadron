d4aae4839cbfe6438ecf56dce48964ab1683aa5f
Carsten Urbach <curbach@gmx.de>
2009-08-13 20:16:19 +0000

    ...

--- src/cdh.h
+++ src/cdh.h
@@ -5,5 +5,5 @@ double g1(double x);
 int g1array(double * x, double * res, const int n);
 void fscdh(double rev, double aLamb1, double aLamb2, double aLamb3, double aLamb4,
 	   double aF0_, double a_fm, int * L, double * ampiV, double *afpiV, const int n,
-	   double * mpiFV, double * fpiFV);
+	   double * mpiFV, double * fpiFV, const int printit);
 #endif
