6d34c0df97677cce6e5b24cc3b91e1366b82a6ea
Siebren Reker <siebren.reker@gmail.com>
2009-11-18 13:19:13 +0000

    Added a simple one-line puttogether script for online measurements, thanks to Carsten.

---
+++ exec/putonlinetogether.sh
@@ -0,0 +1,3 @@
+# $Id$
+
+ls onlinemeas* | sort -k 2 -t "." | xargs grep "^[0-2][ 0-9] ." | grep -v "^21" | sed -e 's/:/ /g' | sed -e 's/onlinemeas\.//g' | awk '{print $2 " " $3 " " $4 " " $5 " " $6 " " $1'} > piononline.dat
