35708bbc6e22cb70865440c3255c6550e4efc614
Carsten Urbach <curbach@gmx.de>
2013-06-29 18:35:18 +0200

    added functionality to extrac dedicated gamma matrix combinations

--- R/readutils.R
+++ R/readutils.R
@@ -5,26 +5,53 @@ readcmicor <- function(filename) {
   return(invisible(data))
 }
 
-readcmidatafiles <- function(path=".", basename="onlinemeas", skip=1, ind.vec=c(1,2,3,4,5),
-                             last.digits=4, verbose=FALSE, sym.vec) {
+readcmidatafiles <- function(path="./", basename="onlinemeas", skip=1,
+                             last.digits=4, verbose=FALSE) {
   ## read one file to determine parameters
   ofiles <- dir(path=path, pattern=paste(basename, "*", sep=""))
-  tmp <- read.table(paste(path, ofiles[1], sep=""), skip=skip)
-  nrObs <- max(tmp[,ind.vec[1]])
-  if(nrObs != length(unique(tmp[,ind.vec[1]]))) {
-    stop("data inconsistent, nrObs not equal to input data length\n")
-  }
-  nrStypes <- length(unique(tmp[,ind.vec[2]]))
-  Time <-  2*max(tmp[,ind.vec[3]])
-  Thalf <- max(tmp[,ind.vec[3]])+1
-  if(Thalf != length(unique(tmp[,ind.vec[3]]))) {
-    stop("data inconsistent, T not equal to input data length\n")
-  }
-  if(verbose) cat("nrObs=",nrObs, "nrStypes=",nrStypes, "T=", Time, "#files=", length(ofiles), "\n")
 
-  ## memory for correlators
-  cf <- array(0, dim=c(length(ofiles), nrObs*Thalf*nrStypes))
+  ## sort input files using the last 4 characters of the filename
+  e <- nchar(ofiles[1])
+  s <- e-last.digits+1
+  ## sort-index vector
+  gaugeno <- as.integer(substr(ofiles,s,e))
+  ii <- order(gaugeno)
 
+  ## read single files
+  j <- 1
+  cmicor <- data.frame()
+  for(i in ii) {
+    if(verbose) {
+      cat("Reading from file", ofiles[i], "\n")
+    }
+    cmicor <- rbind(cmicor, read.table(paste(path, ofiles[i], sep=""), skip=skip))
+    j <- j+1
+  }
+  attr(cmicor, "class") <- c("cmicor", "data.frame")  
+  return(invisible(cmicor))
+}
+
+extract.obs <- function(cmicor, vec.obs=c(1), ind.vec=c(1,2,3,4,5), sym.vec, verbose=FALSE) {
+  if(missing(cmicor)) {
+    stop("data missing in extract.obs\n")
+  }
+  
+  nrObs <- max(cmicor[,ind.vec[1]])
+  if(nrObs != length(unique(cmicor[,ind.vec[1]]))) {
+    stop("extract.obs: data inconsistent, nrObs not equal to input data length\n")
+  }
+  nrStypes <- length(unique(cmicor[,ind.vec[2]]))
+  Time <-  2*max(cmicor[,ind.vec[3]])
+  Thalf <- max(cmicor[,ind.vec[3]])+1
+  if(Thalf != length(unique(cmicor[,ind.vec[3]]))) {
+    stop("extract.obs: data inconsistent, T not equal to input data length\n")
+  }
+  if(verbose) cat("nrObs=",nrObs, "nrStypes=",nrStypes, "T=", Time, "\n")
+
+  nrObs <- length(vec.obs)
+  data <- cmicor[cmicor[,ind.vec[1]] %in% vec.obs,]
+  data[(data[,ind.vec[3]]!=0 & (data[,ind.vec[3]]!=(Thalf-1))),ind.vec[c(4,5)]] <-
+      data[(data[,ind.vec[3]]!=0 & (data[,ind.vec[3]]!=(Thalf-1))),ind.vec[c(4,5)]]/2
   ## symmetrise or anti-symmetrise for given observable?
   isym.vec <- rep(+1, times= nrObs*Thalf*nrStypes)
   if(!missing(sym.vec)) {
@@ -37,28 +64,10 @@ readcmidatafiles <- function(path=".", basename="onlinemeas", skip=1, ind.vec=c(
       }
     }
   }
+  cf <- array((data[,ind.vec[4]] + isym.vec*data[,ind.vec[5]]),
+              dim=c(nrObs*Thalf*nrStypes, length(data[,1])/(nrObs*Thalf*nrStypes)))
 
-  ## sort input files using the last 4 characters of the filename
-  e <- nchar(ofiles[1])
-  s <- e-last.digits+1
-  ## sort-index vector
-  gaugeno <- as.integer(substr(ofiles,s,e))
-  ii <- order(gaugeno)
-
-  ## read single files
-  j <- 1
-  for(i in ii) {
-    if(verbose) {
-      cat("Reading from file", ofiles[i], "\n")
-    }
-    tmp <- read.table(paste(path, ofiles[i], sep=""), skip=skip)
-    ## take care of the zeroth and last times, which don't need to be averaged
-    tmp[(tmp[,ind.vec[3]]!=0 & (tmp[,ind.vec[3]]!=(Thalf-1))),] <-
-      tmp[(tmp[,ind.vec[3]]!=0 & (tmp[,ind.vec[3]]!=(Thalf-1))),]/2
-    cf[j,] <- (tmp[,ind.vec[4]] + isym.vec*tmp[,ind.vec[5]])
-    j <- j+1
-  }
-  return(invisible(list(cf=cf, gaugeno=gaugeno[ii], Time=Time, nrStypes=nrStypes, nrObs=nrObs)))
+  return(invisible(list(cf=cf, Time=Time, nrStypes=nrStypes, nrObs=nrObs)))
 }
 
 readhlcor <- function(filename) {
