1f484f66af61f35fee211e45ab583333e95ca81e
Carsten Urbach <curbach@gmx.de>
2014-03-09 18:56:05 +0100

    implemented correct treatment of NAs in the bootstrap samples

--- R/effectivemass.R
+++ R/effectivemass.R
@@ -68,7 +68,7 @@ bootstrap.effectivemass <- function(cf, boot.R=400, boot.l=20, seed=12345, type=
   return(ret)
 }
 
-fit.effectivemass <- function(cf, t1, t2, useCov=FALSE) {
+fit.effectivemass <- function(cf, t1, t2, useCov=FALSE, replace.na=TRUE) {
   if(missing(cf) || !any(class(cf) == "effectivemass" )) {
     stop("cf is missing or must be of class \"effectivemass\"! Aborting...!\n")
   }
@@ -78,7 +78,7 @@ fit.effectivemass <- function(cf, t1, t2, useCov=FALSE) {
   cf$t1 <- t1
   cf$t2 <- t2
   cf$useCov <- useCov
-  cf$dof <-  cf$nrObs*(t2-t1+1)-1
+  cf$replace.na <- replace.na
   attach(cf)
   
   ## create an index array for the fit range
@@ -88,6 +88,11 @@ fit.effectivemass <- function(cf, t1, t2, useCov=FALSE) {
   for(i in 1:nrObs) {
     ii <- c(ii, ((i-1)*Time/2+t1+1):((i-1)*Time/2+t2+1))
   }
+  ## get rid of the NAs for the fit
+  ii.na <- which(is.na(effMass[ii]))
+  ii <- ii[-ii.na]
+  cf$ii <- ii
+  cf$dof <-  length(ii)-1
 
   ## here we generate the inverse covariance matrix, if required
   ## otherwise take inverse errors squared
@@ -100,22 +105,33 @@ fit.effectivemass <- function(cf, t1, t2, useCov=FALSE) {
 
   cf$invCovMatrix <- M
   par <- c(effMass[t1])
-  opt.res <- optim(par, fn = function(par, y, M) { sum((y-par[1]) %*% M %*% (y-par[1]), na.rm=TRUE)},
+  opt.res <- optim(par, fn = function(par, y, M) { sum((y-par[1]) %*% M %*% (y-par[1]))},
                    method="BFGS", M=M, y = effMass[ii])
-  opt.res <- optim(opt.res$par, fn = function(par, y, M) { sum((y-par[1]) %*% M %*% (y-par[1]), na.rm=TRUE)},
+  opt.res <- optim(opt.res$par, fn = function(par, y, M) { sum((y-par[1]) %*% M %*% (y-par[1]))},
                    control=list(parscale=1/opt.res$par),
                    method="BFGS", M=M, y = effMass[ii])
   par <- opt.res$par
   ## now we bootstrap the fit
   massfit.tsboot <- array(0, dim=c(boot.R, 2))
+
+  ## now we replace all NAs by randomly chosen other bootstrap values
+  tb.save <-  effMass.tsboot
+  if(replace.na) {
+    for(k in ii) {
+      jj <- which(is.na(effMass.tsboot[,k]))
+      rj <- sample.int(n=length(effMass.tsboot[jj, k]), size=length(jj), replace=FALSE)
+      effMass.tsboot[jj, k] <- effMass.tsboot[rj, k]
+    }
+  }
   for(i in 1:boot.R) {
-    opt <- optim(par, fn = function(par, y, M) { sum((y-par[1]) %*% M %*% (y-par[1]), na.rm=TRUE)},
+    opt <- optim(par, fn = function(par, y, M) { return(sum((y-par[1]) %*% M %*% (y-par[1])))},
                  control=list(parscale=1/par),
                  method="BFGS", M=M, y = effMass.tsboot[i,ii])
     massfit.tsboot[i, 1] <- opt$par[1]
     massfit.tsboot[i, 2] <- opt$value
   }
-
+  effMass.tsboot <- tb.save
+  rm(tb.save)
   detach(cf)
   cf$massfit.tsboot <- massfit.tsboot
   cf$opt.res <- opt.res
@@ -124,41 +140,41 @@ fit.effectivemass <- function(cf, t1, t2, useCov=FALSE) {
 }
 
 summary.effectivemass <- function(effMass) {
-  attach(effMass)
   cat("\n ** effective mass values **\n\n")
-  cat("no. measurements\t=\t", N, "\n")
-  cat("boot.R\t=\t", boot.R, "\n")
-  cat("boot.l\t=\t", boot.l, "\n")
-  cat("Time extend\t=\t", Time, "\n")
+  cat("no. measurements\t=\t", effMass$N, "\n")
+  cat("boot.R\t=\t", effMass$boot.R, "\n")
+  cat("boot.l\t=\t", effMass$boot.l, "\n")
+  cat("Time extend\t=\t", effMass$Time, "\n")
+  cat("total NA count in bootstrap samples:\t", length(which(is.na(effMass$effMass.tsboot))), "\n")
   cat("values with errors:\n\n")
-  print(data.frame(t= t, m = effMass, dm = deffMass))
-  detach(effMass)
+  print(data.frame(t= effMass$t, m = effMass$effMass, dm = effMass$deffMass))
 }
 
 summary.effectivemassfit <- function(effMass, verbose=FALSE) {
-  attach(effMass)
   cat("\n ** Result of effective mass analysis **\n\n")
-  cat("no. measurements\t=\t", N, "\n")
-  cat("type\t=\t", type, "\n")
-  cat("boot.R\t=\t", boot.R, "\n")
-  cat("boot.l\t=\t", boot.l, "\n")
-  cat("Time extend\t=\t", Time, "\n")
-  cat("time range from", t1, " to ", t2, "\n")
-  cat("No correlation functions", nrObs, "\n")
+  cat("no. measurements\t=\t", effMass$N, "\n")
+  cat("type\t=\t", effMass$type, "\n")
+  cat("boot.R\t=\t", effMass$boot.R, "\n")
+  cat("boot.l\t=\t", effMass$boot.l, "\n")
+  cat("Time extend\t=\t", effMass$Time, "\n")
+  cat("NA count in fitted bootstrap samples:\t", length(which(is.na(effMass$effMass.tsboot[,effMass$ii]))),
+      "(",length(which(is.na(effMass$effMass.tsboot[,effMass$ii])))/ length(effMass$effMass.tsboot[,effMass$ii]), "%)\n")
+  cat("NAs replaced in fit:", effMass$replace.na, "\n")
+  cat("time range from", effMass$t1, " to ", effMass$t2, "\n")
+  cat("No correlation functions", effMass$nrObs, "\n")
   if(verbose) {
     cat("values with errors:\n\n")
-    print(data.frame(t= t, m = effMass, dm = deffMass))
+    print(data.frame(t= effMass$t, m = effMass$effMass, dm = effMass$deffMass))
   }
-  cat("correlated fit\t=\t", useCov, "\n")
-  cat("m\t=\t", opt.res$par[1], "\n")
-  cat("dm\t=\t", sd(massfit.tsboot[,1]), "\n")
-  cat("chisqr\t=\t", opt.res$value, "\n")
-  cat("dof\t=\t", dof, "\n")
+  cat("correlated fit\t=\t", effMass$useCov, "\n")
+  cat("m\t=\t", effMass$opt.res$par[1], "\n")
+  cat("dm\t=\t", sd(effMass$massfit.tsboot[,1]), "\n")
+  cat("chisqr\t=\t", effMass$opt.res$value, "\n")
+  cat("dof\t=\t", effMass$dof, "\n")
   cat("chisqr/dof=\t",
-      opt.res$value/dof, "\n")
-  cat("Quality of the fit (p-value):",   1-pchisq(opt.res$value, dof), "\n")
+      effMass$opt.res$value/effMass$dof, "\n")
+  cat("Quality of the fit (p-value):",   1-pchisq(effMass$opt.res$value, effMass$dof), "\n")
 
-  detach(effMass)
 }
 
 print.effectivemassfit <- function(effMass, verbose=FALSE) {
