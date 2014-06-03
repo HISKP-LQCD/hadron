64bd82bf5e1f53f85fe6ecceb7d320029c9fc329
Bartosz Kostrzewa <bartosz.kostrzewa@desy.de>
2014-06-03 15:00:04 +0200

    computefps.R: add option to use fps definition with reduced lattice artefacts

--- R/computefps.R
+++ R/computefps.R
@@ -1,32 +1,51 @@
-computefps <- function(mfit, PP, mass, mu1, mu2, Kappa, normalisation="cmi") {
+computefps <- function(mfit, PP, mass, mu1, mu2, Kappa, normalisation="cmi", reduce.latarts=FALSE) {
   if(missing(mu1)) {
     stop("computefps: mu1  must be specified! Aborting...\n")
   }
   if(missing(mu2)) mu2 <- mu1
   if(missing(mfit)) {
-    if(normalisation != "cmi") return((mu1+mu2)/sqrt(2)*abs(PP)/sqrt(mass^3))
-    else return(2*Kappa*(mu1+mu2)/sqrt(2)*abs(PP)/sqrt(mass^3))
+    denominator <- sqrt(mass^3)
+    if(reduce.latarts) denominator <- sinh(mass)*sqrt(mass)
+    
+    if(normalisation != "cmi") return((mu1+mu2)/sqrt(2)*abs(PP)/denominator)
+    else return(2*Kappa*(mu1+mu2)/sqrt(2)*abs(PP)/denominator)
   }
   else if(any(class(mfit) == "matrixfit")) {
     k <- Kappa
     if(normalisation != "cmi") k <- 0.5
     else  mfit$kappa <- Kappa
-    mfit$fps <- 2*k*(mu1+mu2)/sqrt(2)*abs(mfit$opt.res$par[2])/sqrt(mfit$opt.res$par[1]^3)
-    mfit$fps.tsboot <- 2*k*(mu1+mu2)/sqrt(2)*abs(mfit$opt.tsboot[2,])/sqrt(mfit$opt.tsboot[1,]^3)
+    
+    denominator <- sqrt(mfit$opt.res$par[1]^3)
+    if(reduce.latarts) denominator <- sinh(mfit$opt.res$par[1])*sqrt(mfit$opt.res$par[1])
+    mfit$fps <- 2*k*(mu1+mu2)/sqrt(2)*abs(mfit$opt.res$par[2])/denominator
+
+    denominator <- sqrt(mfit$opt.tsboot[1,]^3)
+    if(reduce.latarts) denominator <- sinh(mfit$opt.tsboot[1,])*sqrt(mfit$opt.tsboot[1,])
+    mfit$fps.tsboot <- 2*k*(mu1+mu2)/sqrt(2)*abs(mfit$opt.tsboot[2,])/denominator
+
     mfit$mu1 <- mu1
     mfit$mu2 <- mu2
     mfit$normalisation <- normalisation
+    mfit$reduce.latarts <- reduce.latarts
     return(invisible(mfit))
   }
   else if(inherits(mfit, "gevp.amplitude")) {
     k <- Kappa
     if(normalisation != "cmi") k <- 0.5
     else  mfit$kappa <- Kappa
-    mfit$fps <- 2*k*(mu1+mu2)/sqrt(2)*abs(mfit$meanAmplitude)/sqrt(mfit$m0^3)
-    mfit$fps.tsboot <- 2*k*(mu1+mu2)/sqrt(2)*abs(mfit$meanAmplitude.tsboot[,1])/sqrt(mfit$m0.tsboot^3)
+
+    denominator <- sqrt(mfit$m0^3)
+    if(reduce.latarts) denominator <- sinh(mfit$m0)*sqrt(mfit$m0)
+    mfit$fps <- 2*k*(mu1+mu2)/sqrt(2)*abs(mfit$meanAmplitude)/denominator
+
+    denominator <- sqrt(mfit$m0.tsboot^3)
+    if(reduce.latarts) denominator <- sinh(mfit$m0.tsboot)*sqrt(mfit$m0.tsboot)
+    mfit$fps.tsboot <- 2*k*(mu1+mu2)/sqrt(2)*abs(mfit$meanAmplitude.tsboot[,1])/denominator
+    
     mfit$mu1 <- mu1
     mfit$mu2 <- mu2
     mfit$normalisation <- normalisation
+    mfit$reduce.latarts <- reduce.latarts
     return(invisible(mfit))
   }
   else{
