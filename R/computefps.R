b353dbeebd51525159137924b60be93ca6479f2e
Bartosz Kostrzewa <bartosz.kostrzewa@desy.de>
2014-07-09 12:06:36 +0200

    computefps: rename 'reduce.latarts' into 'disprel' to make it explicit that this form stems from the lattice dispersion relation and doesn't necessarily reduce the effect of lattice artefacts

--- R/computefps.R
+++ R/computefps.R
@@ -1,4 +1,4 @@
-computefps <- function(mfit, PP, mass, mu1, mu2, Kappa, normalisation="cmi", reduce.latarts=FALSE) {
+computefps <- function(mfit, PP, mass, mu1, mu2, Kappa, normalisation="cmi", disprel="continuum") {
   if(missing(mu1)) {
     stop("computefps: mu1  must be specified! Aborting...\n")
   }
@@ -8,7 +8,7 @@ computefps <- function(mfit, PP, mass, mu1, mu2, Kappa, normalisation="cmi", red
   if(missing(mu2)) mu2 <- mu1
   if(missing(mfit)) {
     denominator <- sqrt(mass^3)
-    if(reduce.latarts) denominator <- sinh(mass)*sqrt(mass)
+    if(disprel == "lattice") denominator <- sinh(mass)*sqrt(mass)
     
     if(normalisation != "cmi") return((mu1+mu2)*abs(PP)/denominator)
     else return(sqrt(2)*Kappa*(mu1+mu2)*abs(PP)/denominator)
@@ -19,17 +19,17 @@ computefps <- function(mfit, PP, mass, mu1, mu2, Kappa, normalisation="cmi", red
     else  mfit$kappa <- Kappa
     
     denominator <- sqrt(mfit$opt.res$par[1]^3)
-    if(reduce.latarts) denominator <- sinh(mfit$opt.res$par[1])*sqrt(mfit$opt.res$par[1])
+    if(disprel == "lattice") denominator <- sinh(mfit$opt.res$par[1])*sqrt(mfit$opt.res$par[1])
     mfit$fps <- 2*k*(mu1+mu2)/sqrt(2)*abs(mfit$opt.res$par[2])/denominator
 
     denominator <- sqrt(mfit$opt.tsboot[1,]^3)
-    if(reduce.latarts) denominator <- sinh(mfit$opt.tsboot[1,])*sqrt(mfit$opt.tsboot[1,])
+    if(disprel == "lattice") denominator <- sinh(mfit$opt.tsboot[1,])*sqrt(mfit$opt.tsboot[1,])
     mfit$fps.tsboot <- sqrt(2.)*k*(mu1+mu2)*abs(mfit$opt.tsboot[2,])/denominator
 
     mfit$mu1 <- mu1
     mfit$mu2 <- mu2
     mfit$normalisation <- normalisation
-    mfit$reduce.latarts <- reduce.latarts
+    mfit$disprel <- disprel
     return(invisible(mfit))
   }
   else if(inherits(mfit, "gevp.amplitude")) {
@@ -38,17 +38,17 @@ computefps <- function(mfit, PP, mass, mu1, mu2, Kappa, normalisation="cmi", red
     else  mfit$kappa <- Kappa
 
     denominator <- sqrt(mfit$m0^3)
-    if(reduce.latarts) denominator <- sinh(mfit$m0)*sqrt(mfit$m0)
+    if(disprel == "lattice") denominator <- sinh(mfit$m0)*sqrt(mfit$m0)
     mfit$fps <- sqrt(2)*k*(mu1+mu2)*abs(mfit$meanAmplitude)/denominator
 
     denominator <- sqrt(mfit$m0.tsboot^3)
-    if(reduce.latarts) denominator <- sinh(mfit$m0.tsboot)*sqrt(mfit$m0.tsboot)
+    if(disprel == "lattice") denominator <- sinh(mfit$m0.tsboot)*sqrt(mfit$m0.tsboot)
     mfit$fps.tsboot <- sqrt(2)*k*(mu1+mu2)*abs(mfit$meanAmplitude.tsboot[,1])/denominator
     
     mfit$mu1 <- mu1
     mfit$mu2 <- mu2
     mfit$normalisation <- normalisation
-    mfit$reduce.latarts <- reduce.latarts
+    mfit$disprel <- disprel
     return(invisible(mfit))
   }
   else{
