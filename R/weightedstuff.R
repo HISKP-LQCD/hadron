d5aaafe7b9df20a69411960388486e16bd540b7a
Carsten Urbach <curbach@gmx.de>
2015-01-28 12:11:44 +0100

    added properly working weighted.hist and docu for weighted.median

--- R/weightedstuff.R
+++ R/weightedstuff.R
@@ -4,12 +4,19 @@ weighted.quantile <- function(x, w, probs = seq(0, 1, 0.25), na.rm = FALSE, name
   ## - if w exists (length(w)>0) => if not, then w <- rep(1,length(x))
   ## - if w is the weight or the inclusion probability
   ## + a test sum(w) > length(x)??????
+  if(missing(x)) {
+    stop("Usage: weighted.quantile(x,...) vector of values x required")    
+  }
+  if (missing(w))
+    w <- rep(1, length(x))
+
   if (length(x) != length(w))
     stop("Weights and variable vectors are of unequal length!")
-  if (na.rm)
-    { valid.obs <- !is.na(x) & !is.na(w)
-      x <- x[valid.obs]
-      w <- w[valid.obs]}
+  if (na.rm) {
+    valid.obs <- !is.na(x) & !is.na(w)
+    x <- x[valid.obs]
+    w <- w[valid.obs]
+  }
   else if (any(is.na(x)) | any(is.na(w)))
     stop("Missing values and NaN's not allowed if `na.rm' is FALSE")
   if (any((p.ok <- !is.na(probs)) & (probs < 0 | probs > 1)))
@@ -19,12 +26,7 @@ weighted.quantile <- function(x, w, probs = seq(0, 1, 0.25), na.rm = FALSE, name
     probs <- probs[p.ok]
   }
   np <- length(probs)
-  if (missing(w))
-    w <- rep(1, length(x))
-  if (na.rm) {
-    w <- w[i <- !is.na(x)]
-    x <- x[i]
-  }
+
   ## check if weights are probs or unit correspondences
   ind <- order(x)
   cumw <- cumsum(w[ind])/sum(w)
@@ -269,4 +271,81 @@ weighted.variance <- function(x, w, na.rm=FALSE) {
   v2 <- sum(w^2)
   v1 <- sum(w)
   return ( v1/(v1^2-v2) * sum( w*(x-mustar)^2 ) )
+}
+
+
+get.breaks<-function(x,breaks) {
+                                        # if a break computing function name is passed
+  if(is.character(breaks)) 
+    nbreaks<-do.call(paste("nclass",breaks,sep=".",collapse=""),list(x))
+                                        # if breaks is numeric
+  if(is.numeric(breaks)) {
+                                        # if just the number of breaks is passed
+    if(length(breaks) == 1) {
+      nbreaks<-breaks
+    }
+                                        # otherwise assume that breaks specifies the breakpoints
+    else return(breaks)
+  }
+  breakinc<-diff(range(x))/nbreaks
+  breaks<-c(min(x),rep(breakinc,nbreaks))
+  breaks<-cumsum(breaks)
+  return(breaks)
+}
+
+weighted.hist<-function(x, w, breaks="Sturges",col=NULL,plot=TRUE,
+                        freq=TRUE,ylim=NA,ylab=NULL,xaxis=TRUE,na.rm=FALSE,...) {
+  
+  if(missing(x))
+    stop("Usage: weighted.hist(x,...) vector of values x required")
+  if(missing(w)) w<-rep(1,length(x))
+  if (length(x) != length(w))
+    stop("Weights and variable vectors are of unequal length!")
+  
+  if(na.rm) {
+    valid.obs <- !is.na(x) & !is.na(w)
+    x <- x[valid.obs]
+    w <- w[valid.obs]
+  }
+  else if (any(is.na(x)) | any(is.na(w)))
+    stop("Missing values and NaN's not allowed if `na.rm' is FALSE")
+
+  breaks<-get.breaks(x,breaks)
+  width<-diff(breaks)
+  diffx<-diff(range(x))
+  equidist<-sum(width-width[1]) < diffx/1000
+  nbreaks<-length(breaks)-1
+                                        # make sure that the last break is greater than the maximum value
+  lastbreak<-breaks[nbreaks+1]
+  breaks[nbreaks+1]<-breaks[nbreaks+1]+diffx/1000
+  if(diff(range(breaks)) < diffx)
+    warning("Not all values will be included in the histogram")
+  counts<-rep(0,nbreaks)
+  for(bin in 1:nbreaks)
+    counts[bin]<-sum(w[x >= breaks[bin] & x < breaks[bin+1]])
+  density<-counts/sum(counts)
+  if(freq) {
+    if(is.null(ylab)) ylab<-"Frequency"
+    heights<-counts
+    if(!equidist) 
+      warning("Areas will not relate to frequencies")
+  }
+  else {
+    if(!equidist) {
+      heights<-density*mean(width)/width
+      heights<-heights/sum(heights)
+    }
+    else heights<-density
+    if(is.null(ylab)) ylab<-"Density"
+  }
+  if(plot) {
+    if(is.null(col)) col<-par("bg")
+    if(is.na(ylim)) ylim<-c(0,1.1*max(heights,na.rm=TRUE))
+    mids<-barplot(heights,width=width,col=col,space=0,ylim=ylim,ylab=ylab,...)
+    tickpos<-c(mids-width/2,mids[length(mids)]+width[length(width)]/2)
+    if(xaxis) axis(1,at=tickpos,labels=signif(c(breaks[1:nbreaks],lastbreak),3))
+  }
+  else mids<-breaks[-length(breaks)]+width/2
+  invisible(list(breaks=breaks,counts=counts,density=density,
+                 mids=mids,xname=deparse(substitute(x)),equidist=equidist))
 }
