2a2fa8f0ac90a4c2cc51bc4d8a716f78d216924c
Carsten Urbach <curbach@gmx.de>
2015-01-29 21:29:12 +0100

    scripts for phaseshift improved and finite size corrections for qcotdelta implemented

--- R/fs.mpia0.R
+++ R/fs.mpia0.R
@@ -1,4 +1,24 @@
+## finite size correction to q cot(delta) in the I=2 pipi scattering
+## case from Ref. hep-lat/0601033
+##
+## this formula is valid near threshold only!
+##
+## The difference is defined as Delta = FV - Vinfty
 
+## this is the original aymptotic formula Eq (31) from Ref.
+## hep-lat/0601033
+## which can be applied to a0 by using the effective range expansion
+## q*cot(delta) = 1/(-a0)
+fs.qcotdelta <- function(mps, L) {
+  ## the 7th is zero, so we skip sqrt(7)
+  cn <- c(6, 12, 8, 6, 24, 24, 12)
+  n <- c(1, sqrt(2), sqrt(3), 2, sqrt(5), sqrt(6), sqrt(8))
+  return(-mps/sqrt(2*pi)*sum( cn*exp(-n*mps*L)/sqrt(n*mps*L)*(1-227/(24*n*mps*L)) ))
+}
+
+## this is the formula from
+## arXiv:0909.3255
+## directly for mpi*a0
 fs.mpia0 <- function(mps, fps, L) {
 
   fn <- function(n, cn, mpsL) {
@@ -10,3 +30,4 @@ fs.mpia0 <- function(mps, fps, L) {
 
   return(mps^4/fps^4/2^(13/2)/pi^(5/2)*S)
 }
+
--- exec/phaseshift/summary.R
+++ exec/phaseshift/summary.R
@@ -1,3 +1,4 @@
+library(plotrix)
 load(paste("res.", pc, ".", TP, ".Rdata", sep=""))
 
 replace.outliers <- function(x, lh, rh) {
@@ -8,7 +9,7 @@ replace.outliers <- function(x, lh, rh) {
 
 compute.weights <- function(err, pvalues) {
 ##  return(1./pvalues^2)
-  return(pvalues^2 * min(err)^2/err^2)
+  return(pvalues^2 * min(err, na.rm=TRUE)^2/err^2)
 }
 
 ii <- which(dim(res) == 1)-1
@@ -32,20 +33,32 @@ err <- as.vector(apply(res[,,,3]/res[,,,5], jj, sd, na.rm=TRUE))
 pvalues <- as.vector((1-2*abs(res[1,,,7]-0.5)) * (1-2*abs(res[1,,,8]-0.5)))
 ## weights
 w <- compute.weights(err, pvalues)
-qcotdeltaovmpi <- weighted.quantile(as.vector(res[1,,,3]/res[1,,,5]), prob=c(0.5), w=w, na.rm=TRUE)
 
+qcotdeltaovmpi <- weighted.quantile(as.vector(res[1,,,3]/res[1,,,5]), prob=c(0.5), w=w, na.rm=TRUE)
+if(any(is.na(as.vector(res[1,,,3]/res[1,,,5])))) {
+  weighted.hist(as.vector(res[1,,,3]/res[1,,,5])[-which(is.na(as.vector(res[1,,,3]/res[1,,,5])))], w=w[-which(is.na(as.vector(res[1,,,3]/res[1,,,5])))], main="qcotdeltaovmpi")
+}
+if(!any(is.na(as.vector(res[1,,,3]/res[1,,,5])))) {
+  weighted.hist(as.vector(res[1,,,3]/res[1,,,5]), w=w, main="qcotdeltaovmpi")
+}
 ## statistical error
 qcotdeltaovmpi[2] <- sd(apply(res[,,,3]/res[,,,5], 1, weighted.quantile, prob=c(0.5), w=w, na.rm=TRUE), na.rm=TRUE)
 ## systematic error
 ## lower and upper
-qcotdeltaovmpi[c(3:4)] <- weighted.quantile(as.vector(res[1,,,3]/res[1,,,5]), w=w, prob=c(0.1573), na.rm=TRUE)-qcotdeltaovmpi[1]
+qcotdeltaovmpi[c(3:4)] <- weighted.quantile(as.vector(res[1,,,3]/res[1,,,5]), w=w, prob=c(0.1573, 0.8427), na.rm=TRUE)-qcotdeltaovmpi[1]
 
 ## this is the naive statistical uncertainty
 err <- as.vector(apply(res[,,,3], jj, sd, na.rm=TRUE))
 ## weights
 w <- compute.weights(err, pvalues)
-qcotdelta <- weighted.quantile(as.vector(res[1,,,3]), w=w, prob=c(0.5), na.rm=TRUE)
 
+qcotdelta <- weighted.quantile(as.vector(res[1,,,3]), w=w, prob=c(0.5), na.rm=TRUE)
+if(any(is.na(as.vector(res[1,,,3])))) {
+  weighted.hist(as.vector(res[1,,,3])[-which(is.na(as.vector(res[1,,,3])))], w=w[-which(is.na(as.vector(res[1,,,3])))], main="qcotdelta")
+}
+if(!any(is.na(as.vector(res[1,,,3])))) {
+  weighted.hist(as.vector(res[1,,,3]), w=w, main="qcotdelta")
+}
 ## statistical error
 qcotdelta[2] <- sd(apply(res[,,,3], 1, weighted.quantile, w=w, prob=c(0.5), na.rm=TRUE), na.rm=TRUE)
 ## systematic error
@@ -56,8 +69,14 @@ qcotdelta[c(3:4)] <- weighted.quantile(as.vector(res[1,,,3]), w=w, prob=c(0.1573
 err <- as.vector(apply(res[,,,1]/res[,,,5]^2, jj, sd, na.rm=TRUE))
 ## q^2/mpi^2
 w <- compute.weights(err, pvalues)
-qsqovmpisq <- weighted.quantile(as.vector(res[1,,,1]/res[1,,,5]^2), w=w, prob=c(0.5), na.rm=TRUE)
 
+qsqovmpisq <- weighted.quantile(as.vector(res[1,,,1]/res[1,,,5]^2), w=w, prob=c(0.5), na.rm=TRUE)
+if(any(is.na(as.vector(res[1,,,1]/res[1,,,5]^2)))) {
+  weighted.hist(as.vector(res[1,,,1]/res[1,,,5]^2)[-which(is.na(as.vector(res[1,,,1]/res[1,,,5]^2)))], w=w[-which(is.na(as.vector(res[1,,,1]/res[1,,,5]^2)))], main="qsovmpisq")
+}
+if(!any(is.na(as.vector(res[1,,,1]/res[1,,,5]^2)))) {
+  weighted.hist(as.vector(res[1,,,1]/res[1,,,5]^2), w=w, main="qsovmpisq")
+}
 ##qsqovmpisq.statd
 qsqovmpisq[2] <- sd(apply(res[,,,1]/res[,,,5]^2, 1, weighted.quantile, w=w, prob=c(0.5), na.rm=TRUE), na.rm=TRUE)
 qsqovmpisq[c(3:4)] <- weighted.quantile(as.vector(res[1,,,1]/res[1,,,5]^2), w=w, prob=c(0.1573, 0.8427), na.rm=TRUE)-qsqovmpisq[1]
@@ -66,30 +85,73 @@ qsqovmpisq[c(3:4)] <- weighted.quantile(as.vector(res[1,,,1]/res[1,,,5]^2), w=w,
 err <- as.vector(apply(res[,,,1], jj, sd, na.rm=TRUE))
 ## q^2/mpi^2
 w <- compute.weights(err, pvalues)
-qsq <- weighted.quantile(as.vector(res[1,,,1]), w=w, prob=c(0.5), na.rm=TRUE)
 
+qsq <- weighted.quantile(as.vector(res[1,,,1]), w=w, prob=c(0.5), na.rm=TRUE)
+if(any(is.na(as.vector(res[1,,,1])))) {
+  weighted.hist(as.vector(res[1,,,1])[-which(is.na(as.vector(res[1,,,1])))], w=w[-which(is.na(as.vector(res[1,,,1])))], main="qsq")
+}
+if(!any(is.na(as.vector(res[1,,,1])))) {
+  weighted.hist(as.vector(res[1,,,1]), w=w, main="qsq")
+}
 ##qsqovmpisq.statd
 qsq[2] <- sd(apply(res[,,,1], 1, weighted.quantile, w=w, prob=c(0.5), na.rm=TRUE), na.rm=TRUE)
 qsq[c(3:4)] <- weighted.quantile(as.vector(res[1,,,1]), w=w, prob=c(0.1573, 0.8427), na.rm=TRUE)-qsq[1]
 
 ## this is the statistical uncertainty
+err <- as.vector(apply(sqrt(res[,,,1]), jj, sd, na.rm=TRUE))
+## q^2/mpi^2
+w <- compute.weights(err, pvalues)
+
+q <- weighted.quantile(as.vector(sqrt(res[1,,,1])), w=w, prob=c(0.5), na.rm=TRUE)
+if(any(is.na(as.vector(sqrt(res[1,,,1]))))) {
+  weighted.hist(as.vector(sqrt(res[1,,,1]))[-which(is.na(as.vector(sqrt(res[1,,,1]))))], w=w[-which(is.na(as.vector(sqrt(res[1,,,1]))))], main="q")
+}
+if(!any(is.na(as.vector(res[1,,,1])))) {
+  weighted.hist(as.vector(sqrt(res[1,,,1])), w=w, main="q")
+}
+##qsqovmpisq.statd
+q[2] <- sd(apply(sqrt(res[,,,1]), 1, weighted.quantile, w=w, prob=c(0.5), na.rm=TRUE), na.rm=TRUE)
+q[c(3:4)] <- weighted.quantile(as.vector(sqrt(res[1,,,1])), w=w, prob=c(0.1573, 0.8427), na.rm=TRUE)-q[1]
+
+
+## this is the statistical uncertainty
 err <- as.vector(apply(res[,,,4], jj, sd, na.rm=TRUE))
 #delta
 w <- compute.weights(err, pvalues)
-delta <- weighted.quantile(as.vector(res[1,,,4]), w=w, prob=c(0.5), na.rm=TRUE)
 
+delta <- weighted.quantile(as.vector(res[1,,,4]), w=w, prob=c(0.5), na.rm=TRUE)
+if(any(is.na(as.vector(res[1,,,4])))) {
+  weighted.hist(as.vector(res[1,,,4])[-which(is.na(as.vector(res[1,,,4])))], w=w[-which(is.na(as.vector(res[1,,,4])))], main="delta")
+}
+if(!any(is.na(as.vector(res[1,,,4])))) {
+  weighted.hist(as.vector(res[1,,,4]), w=w, main="delta")
+}
 delta[2] <- sd(apply(res[,,,4], 1, weighted.quantile, w=w, prob=c(0.5), na.rm=TRUE), na.rm=TRUE)
 delta[c(3,4)] <- weighted.quantile(as.vector(res[1,,,4]), w=w, prob=c(0.1573, 0.8427), na.rm=TRUE)-delta[1]
 
 err <- as.vector(apply(res[,,,5], jj, sd, na.rm=TRUE))
 w <- compute.weights(err, pvalues)
+
 Epi <- weighted.quantile(as.vector(res[1,,,5]), w=w, prob=c(0.5), na.rm=TRUE)
+if(any(is.na(as.vector(res[1,,,5])))) {
+  weighted.hist(as.vector(res[1,,,5])[-which(is.na(as.vector(res[1,,,5])))], w=w[-which(is.na(as.vector(res[1,,,5])))], main="Epi")
+}
+if(!any(is.na(as.vector(res[1,,,5])))) {
+  weighted.hist(as.vector(res[1,,,5]), w=w, main="Epi")
+}
 Epi[2] <- sd(apply(res[,,,5], 1, weighted.quantile, w=w, prob=c(0.5), na.rm=TRUE), na.rm=TRUE)
 Epi[c(3,4)] <- weighted.quantile(as.vector(res[1,,,5]), w=w, prob=c(0.1573, 0.8427), na.rm=TRUE)-Epi[1]
 
 err <- as.vector(apply(res[,,,6], jj, sd, na.rm=TRUE))
 w <- compute.weights(err, pvalues)
+
 Epipi <- weighted.quantile(as.vector(res[1,,,6]), w=w, prob=c(0.5), na.rm=TRUE)
+if(any(is.na(as.vector(res[1,,,6])))) {
+  weighted.hist(as.vector(res[1,,,6])[-which(is.na(as.vector(res[1,,,6])))], w=w[-which(is.na(as.vector(res[1,,,6])))], main="Epipi")
+}
+if(!any(is.na(as.vector(res[1,,,6])))) {
+  weighted.hist(as.vector(res[1,,,6]), w=w, main="Epipi")
+}
 Epipi[2] <- sd(apply(res[,,,6], 1, weighted.quantile, w=w, prob=c(0.5), na.rm=TRUE), na.rm=TRUE)
 Epipi[c(3,4)] <- weighted.quantile(as.vector(res[1,,,6]), w=w, prob=c(0.1573, 0.8427), na.rm=TRUE)-Epipi[1]
 
