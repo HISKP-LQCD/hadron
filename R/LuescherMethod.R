8de36cd23318b664c1b01348647571ff0c6826ab
Carsten Urbach <curbach@gmx.de>
2015-03-04 10:46:42 +0100

    added Ecm in the returned list

--- R/LuescherMethod.R
+++ R/LuescherMethod.R
@@ -13,13 +13,13 @@ compute.qtildesq <- function(E, dvec, mpi, L) {
   ## cosh(Ecm/2) = 2 sin^2(q*/2) + cosh(mpi)
   q = 2*asin(sqrt( (cosh(Ecm/2) - cosh(mpi))/2. ))
   ## qtsq = 2 pi q / L
-  return(data.frame(gammaboost=E/Ecm, qtsq=(L*q/2./pi)^2, q=q))
+  return(data.frame(gammaboost=E/Ecm, qtsq=(L*q/2./pi)^2, q=q, Ecm=Ecm))
 }
 
 ## version with continuum dispersion relation
 compute.qtildesq.contdisp <- function(E, dvec, mpi, L) {
   Ecmsq <- E^2 - sum((2*pi*dvec/L)^2)
   qsq <- Ecmsq/4.-mpi^2
-  return(data.frame(gammaboost=E/sqrt(Ecmsq), qtsq=qsq*(L/2./pi)^2, q=sqrt(qsq)))
+  return(data.frame(gammaboost=E/sqrt(Ecmsq), qtsq=qsq*(L/2./pi)^2, q=sqrt(qsq), Ecm=sqrt(Ecmsq)))
 }
 
