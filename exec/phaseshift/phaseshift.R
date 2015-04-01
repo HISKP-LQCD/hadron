8d99abb7be28163e1e76d505b9cb06570d729ed1
Carsten Urbach <curbach@gmx.de>
2015-04-01 14:58:46 +0200

    fixed a bug in gammaofqsq

--- exec/phaseshift/phaseshift.R
+++ exec/phaseshift/phaseshift.R
@@ -1,32 +1,36 @@
 ## omega_lm
+## this needs qtilde as input!
 omegalm <- function(l=0, m=0, q, gamma=1, dvec=c(0,0,0)) {
   return( LuescherZeta(qsq = q^2, l=l, m=m, gamma=gamma, dvec=dvec)/(pi^(3/2)*sqrt(2*l+1)*q^(l+1)*gamma) )
 }
 
-Ecmofqsq <- function(qsq, Mpi) {
-  return(2*acosh(cosh(Mpi) +  2*sin(qsq/2)^2 ))
+## center of mass energy from q^2 and Mpi
+Ecmofqsq <- function(q, Mpi) {
+  return(2*acosh(cosh(Mpi) +  2*sin(q/2)^2 ))
 }
 
+## energy level in MF from Ecm
 EofEcm <- function(Ecm, dvec, L) {
   return(acosh( cosh(Ecm) + 2*sum(sin(pi*dvec/L)^2) ))
 }
 
 ## computes the boost factor as a function of q^2
-gammaofqsq <- function(qsq, dvec, Mpi, L) {
-  Ecm <- Ecmofqsq(qsq, Mpi)
+gammaofqsq <- function(q, dvec, Mpi, L) {
+  Ecm <- Ecmofqsq(q, Mpi)
   ## E/Ecm
   return(EofEcm(Ecm, dvec, L)/Ecm)
 }
 
 ## computes the energy level given q^2
-Eofqsq <- function(qsq, dvec, Mpi, L) {
-  Ecm <- Ecmofqsq(qsq, Mpi)
+Eofqsq <- function(q, dvec, Mpi, L) {
+  Ecm <- Ecmofqsq(q, Mpi)
   return(EofEcm(Ecm, dvec, L))
 }
 
 prepdetEqCMscan <- function(q, Mpi, L) {
   W <- list()
-  W$gamma <- rep(1., times=(length(q)))
+  W$dvec <- c(0,0,0)
+  W$gamma <- gammaofqsq(q, dvec=W$dvec, Mpi=Mpi, L=L)
   W$qt <- L*q/2/pi
   W$L <- L
   W$Mpi <- Mpi
@@ -37,19 +41,19 @@ prepdetEqCMscan <- function(q, Mpi, L) {
 }
 
 detEqCMscan <- function(par, W) {
-  return(par[1] + par[2]*W$q^2 - W$q*W$w00)
+  return(par[1] + 0.5*par[2]*W$q^2 - W$q*W$w00)
 }
 
 detEqCM <- function(q, par, Mpi, L) {
   gamma <- rep(1., times=(length(q)))
   qt <- L*q/2/pi
-  return(par[1] + par[2]*q^2 - q*Re(omegalm(l=0, m=0, q=qt, gamma=gamma)))
+  return(par[1] + 0.5*par[2]*q^2 - q*Re(omegalm(l=0, m=0, q=qt, gamma=gamma)))
 }
 
 prepdetEqMF1scan <- function(q, Mpi, L) {
   W <- list()
   W$dvec <- c(0,0,1)
-  W$gamma <- gammaofqsq(q^2, dvec=W$dvec, Mpi=Mpi, L=L)
+  W$gamma <- gammaofqsq(q, dvec=W$dvec, Mpi=Mpi, L=L)
   W$qt <- L*q/2/pi
   W$L <- L
   W$Mpi <- Mpi
@@ -63,7 +67,7 @@ prepdetEqMF1scan <- function(q, Mpi, L) {
 }
 
 detEqMF1scan <- function(par, W) {
-  cd0 <- par[1] +par[2]*W$q^2
+  cd0 <- par[1] + 0.5*par[2]*W$q^2
   cd2 <- par[3]
   return( (cd0 - W$q*W$w00)*
          (cd2 - W$q^5*(W$w00 + 10/7*W$w20 + 18/7*W$w40))
@@ -74,14 +78,14 @@ detEqMF1scan <- function(par, W) {
 detEqMF1 <- function(q, par, Mpi, L) {
   dvec <- c(0,0,1)
 
-  gamma <- gammaofqsq(q^2, dvec=dvec, Mpi=Mpi, L=L)
+  gamma <- gammaofqsq(q, dvec=dvec, Mpi=Mpi, L=L)
 
   qt <- L*q/2/pi
   w00 <- Re(omegalm(l=0, m=0, q=qt, gamma=gamma, dvec=dvec))
   w20 <- Re(omegalm(l=2, m=0, q=qt, gamma=gamma, dvec=dvec))
   w40 <- Re(omegalm(l=4, m=0, q=qt, gamma=gamma, dvec=dvec))
 
-  cd0 <- par[1] +par[2]*q^2
+  cd0 <- par[1] + 0.5*par[2]*q^2
   cd2 <- par[3]
   return( (cd0 - q*w00)*
          (cd2 - q^5*(w00 + 10/7*w20 + 18/7*w40))
@@ -91,8 +95,8 @@ detEqMF1 <- function(q, par, Mpi, L) {
 
 prepdetEqMF2scan <- function(q, Mpi, L) {
   W <- list()
-  W$dvec <- c(0,1,1)
-  W$gamma <- gammaofqsq(q^2, dvec=W$dvec, Mpi=Mpi, L=L)
+  W$dvec <- c(1,1,0)
+  W$gamma <- gammaofqsq(q, dvec=W$dvec, Mpi=Mpi, L=L)
   W$qt <- L*q/2/pi
   W$L <- L
   W$Mpi <- Mpi
@@ -101,52 +105,52 @@ prepdetEqMF2scan <- function(q, Mpi, L) {
   W$w00 <- Re(omegalm(l=0, m=0, q=W$qt, gamma=W$gamma, dvec=W$dvec))
   W$w20 <- Re(omegalm(l=2, m=0, q=W$qt, gamma=W$gamma, dvec=W$dvec))
   W$w40 <- Re(omegalm(l=4, m=0, q=W$qt, gamma=W$gamma, dvec=W$dvec))
-  W$w22 <- Re(omegalm(l=2, m=2, q=W$qt, gamma=W$gamma, dvec=W$dvec))
+  W$w22 <- omegalm(l=2, m=2, q=W$qt, gamma=W$gamma, dvec=W$dvec)
   W$w44 <- Re(omegalm(l=4, m=4, q=W$qt, gamma=W$gamma, dvec=W$dvec))
-  W$w42 <- Re(omegalm(l=4, m=2, q=W$qt, gamma=W$gamma, dvec=W$dvec))
+  W$w42 <- omegalm(l=4, m=2, q=W$qt, gamma=W$gamma, dvec=W$dvec)
 
   return(W)
 }
 
 detEqMF2scan <- function(par, W) {
-  cd0 <- (par[1] + par[2]*W$q^2)/W$q
+  cd0 <- (par[1] + 0.5*par[2]*W$q^2)/W$q
   cd2 <- par[3]/W$q^5
 
-  return(q^11*(10/49*(10*(-2*cd0 + 2*W$w00 + 7*W$w20)*W$w22^2 +
-                      3*sqrt(15)*(4*cd0 - 4*W$w00 - 7*W$w20)*W$w22*W$w42 + 27*(W$w00 - cd0)*W$w42^2)
-               +(-cd2 + W$w00 + 2/7*(5*W$w20 + 9*W$w40))*(10*W$w22^2 + 1/7*(cd0 - W$w00)*(7*cd2 - 7*W$w00 + 10*W$w20 - 3*W$w40 + 3*sqrt(70)*W$w44)) +
-               5/7*W$w20*(20*W$w22^2 - 6*sqrt(15)*W$w22*W$w42 + W$w20*(7*cd2 - 7*W$w00 + 10*W$w20 - 3*W$w40 + 3*sqrt(70)*W$w44))
-               )
-         )
+  return(Re(q^11*(10/49*(10*(-2*cd0 + 2*W$w00 + 7*W$w20)*W$w22^2 +
+                         3*sqrt(15)*(4*cd0 - 4*W$w00 - 7*W$w20)*W$w22*W$w42 + 27*(W$w00 - cd0)*W$w42^2)
+                  +(-cd2 + W$w00 + 2/7*(5*W$w20 + 9*W$w40))*(10*W$w22^2 + 1/7*(cd0 - W$w00)*(7*cd2 - 7*W$w00 + 10*W$w20 - 3*W$w40 + 3*sqrt(70)*W$w44)) +
+                  5/7*W$w20*(20*W$w22^2 - 6*sqrt(15)*W$w22*W$w42 + W$w20*(7*cd2 - 7*W$w00 + 10*W$w20 - 3*W$w40 + 3*sqrt(70)*W$w44))
+                  )
+            ))
 }
 
 detEqMF2 <- function(q, par, Mpi, L) {
-  dvec=c(0,1,1)
-  gamma <- gammaofqsq(q^2, dvec=dvec, Mpi=Mpi, L=L)
+  dvec=c(1,1,0)
+  gamma <- gammaofqsq(q, dvec=dvec, Mpi=Mpi, L=L)
 
   qt <- L*q/2/pi
   w00 <- Re(omegalm(l=0, m=0, q=qt, gamma=gamma, dvec=dvec))
   w20 <- Re(omegalm(l=2, m=0, q=qt, gamma=gamma, dvec=dvec))
-  w22 <- Re(omegalm(l=2, m=2, q=qt, gamma=gamma, dvec=dvec))
+  w22 <- omegalm(l=2, m=2, q=qt, gamma=gamma, dvec=dvec)
   w40 <- Re(omegalm(l=4, m=0, q=qt, gamma=gamma, dvec=dvec))
   w44 <- Re(omegalm(l=4, m=4, q=qt, gamma=gamma, dvec=dvec))
-  w42 <- Re(omegalm(l=4, m=2, q=qt, gamma=gamma, dvec=dvec))
+  w42 <- omegalm(l=4, m=2, q=qt, gamma=gamma, dvec=dvec)
 
-  cd0 <- (par[1] + par[2]*q^2)/q
+  cd0 <- (par[1] + 0.5*par[2]*q^2)/q
   cd2 <- par[3]/q^5
   
-  return(q^11*(10/49*(10*(-2*cd0 + 2*w00 + 7*w20)*w22^2 +
+  return(Re(q^11*(10/49*(10*(-2*cd0 + 2*w00 + 7*w20)*w22^2 +
                       3*sqrt(15)*(4*cd0 - 4*w00 - 7*w20)*w22*w42 + 27*(w00 - cd0)*w42^2)
                +(-cd2 + w00 + 2/7*(5*w20 + 9*w40))*(10*w22^2 + 1/7*(cd0 - w00)*(7*cd2 - 7*w00 + 10*w20 - 3*w40 + 3*sqrt(70)*w44)) +
                5/7*w20*(20*w22^2 - 6*sqrt(15)*w22*w42 + w20*(7*cd2 - 7*w00 + 10*w20 - 3*w40 + 3*sqrt(70)*w44))
                )
-         )
+         ))
 }
 
 prepdetEqMF3scan <- function(q, Mpi, L) {
   W <- list()
   W$dvec <- c(1,1,1)
-  W$gamma <- gammaofqsq(q^2, dvec=W$dvec, Mpi=Mpi, L=L)
+  W$gamma <- gammaofqsq(q, dvec=W$dvec, Mpi=Mpi, L=L)
   W$qt <- L*q/2/pi
   W$L <- L
   W$Mpi <- Mpi
@@ -154,37 +158,39 @@ prepdetEqMF3scan <- function(q, Mpi, L) {
 
   W$w00 <- Re(omegalm(l=0, m=0, q=W$qt, gamma=W$gamma, dvec=W$dvec))
   W$w40 <- Re(omegalm(l=4, m=0, q=W$qt, gamma=W$gamma, dvec=W$dvec))
-  W$w22 <- (omegalm(l=2, m=2, q=W$qt, gamma=W$gamma, dvec=W$dvec))
-  W$w42 <- -Im(omegalm(l=4, m=2, q=W$qt, gamma=W$gamma, dvec=W$dvec))
+  W$w22 <- omegalm(l=2, m=2, q=W$qt, gamma=W$gamma, dvec=W$dvec)
+  W$w42 <- omegalm(l=4, m=2, q=W$qt, gamma=W$gamma, dvec=W$dvec)
 
   return(W)
 }
 
 detEqMF3scan <- function(par, W) {
-  cd0 <- (par[1] + par[2]*W$q^2)
+  cd0 <- (par[1] + 0.5*par[2]*W$q^2)
   cd2 <- par[3]
 
-  return(( cd0 - W$q*W$w00 )*
-         ( cd2 - W$q^5*(W$w00 - 12/7*W$w40 - 12*sqrt(10)/7*W$w42 + 10*sqrt(6)/7*Im(W$w22))) +
-         W$q^6*30*Re(W$w22)^2
+  return(Re(( cd0 - W$q*W$w00 )*
+            ( cd2 - W$q^5*(W$w00 - 12/7*W$w40 - 12*sqrt(10)/7*1i*W$w42 - 10*sqrt(6)/7*1i*W$w22)) +
+            W$q^6*30*W$w22^2
+            )
          )
 }
 
 detEqMF3 <- function(q, par, Mpi, L) {
   dvec=c(1,1,1)
-  gamma <- gammaofqsq(q^2, dvec=dvec, Mpi=Mpi, L=L)
+  gamma <- gammaofqsq(q, dvec=dvec, Mpi=Mpi, L=L)
 
   qt <- L*q/2/pi
   w00 <- Re(omegalm(l=0, m=0, q=qt, gamma=gamma, dvec=dvec))
   w40 <- Re(omegalm(l=4, m=0, q=qt, gamma=gamma, dvec=dvec))
-  w22 <- (omegalm(l=2, m=2, q=qt, gamma=gamma, dvec=dvec))
-  w42 <- -Im(omegalm(l=4, m=2, q=qt, gamma=gamma, dvec=dvec))
+  w22 <- omegalm(l=2, m=2, q=qt, gamma=gamma, dvec=dvec)
+  w42 <- omegalm(l=4, m=2, q=qt, gamma=gamma, dvec=dvec)
 
-  cd0 <- (par[1] + par[2]*q^2)
+  cd0 <- (par[1] + 0.5*par[2]*q^2)
   cd2 <- par[3]
-  return(( cd0 - q*w00 )*
-         ( cd2 - q^5*(w00 - 12/7*w40 - 12*sqrt(10)/7*w42 + 10*sqrt(6)/7*Im(w22))) +
-         q^6*30*Re(w22)^2
+  return(Re(( cd0 - q*w00 )*
+            ( cd2 - q^5*(w00 - 12/7*w40 - 12*sqrt(10)/7*1i*w42 - 10*sqrt(6)/7*1i*w22)) +
+            q^6*30*w22^2
+            )
          )
 }
 
@@ -221,7 +227,7 @@ findZeros <- function(fn, q, ii, makeplot=FALSE, tol=1.e-12, ...) {
     z <- uniroot(fn, interval=c(q[ii[j]], q[ii[j]+1]), tol = tol, ...)
     if(z$f.root < 1) {
       zeros[j] <- z$root
-      cat("root", j, ":", z$root, z$f.root, z$estim.prec, "\n")
+##      cat("root", j, ":", z$root, z$f.root, z$estim.prec, "\n")
     }
   }
   if(makeplot) {
