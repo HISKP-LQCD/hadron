de26572130c98b9daabcbd5b25dba972eb70d383
Bartosz Kostrzewa <bartosz.kostrzewa@desy.de>
2015-04-09 16:04:54 +0200

    tikzutils: tikz.init and tikz.finalize convenience functions for 'tikzDevice'

---
+++ R/tikzutils.R
@@ -0,0 +1,30 @@
+tikz.init <- function(basename,...) {
+  havetikz <- require("tikzDevice")
+  if(!havetikz){
+    stop("tikz.init: tikzDevice package was not found!")
+  }
+
+  temp <- sprintf("%s.%s",basename,c("tex","pdf","aux","log"))
+  tikzfiles <- list(tex=temp[1],pdf=temp[2],aux=temp[3],log=temp[4])
+  tikz(tikzfiles$tex, standAlone = TRUE,...)
+  tikzfiles
+}
+
+tikz.finalize <- function(tikzfiles,crop=TRUE,margins=0,clean=TRUE) {
+  dev.off()
+  tools::texi2dvi(tikzfiles$tex,pdf=T)
+  if(crop){
+    # use pdfcrop tool to remove plot borders
+    if( Sys.which("pdfcrop") != "" ){
+      command <- sprintf("pdfcrop --margins=%s %s %s",margins,tikzfiles$pdf,tikzfiles$pdf)
+      system(command)
+    } else {
+      warning(sprintf("tikz_finalize: crop requested for %s but 'pdfcrop' tool not found!",tikzfiles$pdf))
+    }
+  }
+  if(clean){
+    # remove temporary files 
+    command <- sprintf("rm %s %s %s", tikzfiles$tex, tikzfiles$log, tikzfiles$aux)
+    system(command)
+  }
+}
