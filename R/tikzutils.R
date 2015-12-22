15f74fd50e76b9c7809ce9931c96053a039a8b89
Carsten Urbach <curbach@gmx.de>
2015-12-22 13:23:42 +0100

    add the possibility for non standalone output

--- R/tikzutils.R
+++ R/tikzutils.R
@@ -1,30 +1,33 @@
-tikz.init <- function(basename,...) {
+tikz.init <- function(basename, stadAlone = TRUE, ...) {
   havetikz <- require("tikzDevice")
   if(!havetikz){
     stop("tikz.init: tikzDevice package was not found!")
   }
 
   temp <- sprintf("%s.%s",basename,c("tex","pdf","aux","log"))
-  tikzfiles <- list(tex=temp[1],pdf=temp[2],aux=temp[3],log=temp[4])
-  tikz(tikzfiles$tex, standAlone = TRUE,...)
+  tikzfiles <- list(tex=temp[1], pdf=temp[2], aux=temp[3], log=temp[4], standAlone=standAlone)
+  tikz(tikzfiles$tex, standAlone = standAlone, ...)
   tikzfiles
 }
 
-tikz.finalize <- function(tikzfiles,crop=TRUE,margins=0,clean=TRUE) {
+tikz.finalize <- function(tikzfiles, crop=TRUE, margins=0, clean=TRUE) {
   dev.off()
-  tools::texi2dvi(tikzfiles$tex,pdf=T)
-  if(crop){
-    # use pdfcrop tool to remove plot borders
-    if( Sys.which("pdfcrop") != "" ){
-      command <- sprintf("pdfcrop --margins=%s %s %s",margins,tikzfiles$pdf,tikzfiles$pdf)
-      system(command)
-    } else {
-      warning(sprintf("tikz_finalize: crop requested for %s but 'pdfcrop' tool not found!",tikzfiles$pdf))
+  if(tikzfiles$standAlone) {
+    tools::texi2dvi(tikzfiles$tex, pdf=T)
+    if(crop){
+      ## use pdfcrop tool to remove plot borders
+      if( Sys.which("pdfcrop") != "" ){
+        command <- sprintf("pdfcrop --margins=%s %s %s",margins,tikzfiles$pdf,tikzfiles$pdf)
+        system(command)
+      }
+      else {
+        warning(sprintf("tikz_finalize: crop requested for %s but 'pdfcrop' tool not found!",tikzfiles$pdf))
+      }
     }
-  }
-  if(clean){
-    # remove temporary files 
-    command <- sprintf("rm %s %s %s", tikzfiles$tex, tikzfiles$log, tikzfiles$aux)
-    system(command)
+    if(clean){
+      ## remove temporary files 
+      command <- sprintf("rm %s %s %s", tikzfiles$tex, tikzfiles$log, tikzfiles$aux)
+      system(command)
+    }
   }
 }
