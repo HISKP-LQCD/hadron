4b1d5df7142e2e6357abb0cbac9225d43bd2a727
Carsten Urbach <curbach@gmx.de>
2016-07-19 22:44:06 +0200

    adapted for new rho data

--- exec/rho-phaseshift/summarise.R
+++ exec/rho-phaseshift/summarise.R
@@ -3,58 +3,54 @@ compute.weights <- function(err, pvalues) {
   }
 
 compute.boots <- function(res, index=1) {
-  j <- 5
-  if(index %% 2 == 0) j <- 6
-  pvalues <- as.vector((1-2*abs(res[1,,j]-0.5)))
+  pvalues <- as.vector((1-2*abs(res[1,,4]-0.5)))
   err <- apply(res[,,index], 2, sd, na.rm=TRUE)
   w <- compute.weights(err, pvalues)
   return(apply(res[,,index], 1, weighted.quantile, prob=c(0.5), w=w, na.rm=TRUE))
 }
 
 
-summarise.rho <- function(ens, frame) {
-  filelist <- Sys.glob(paste("./", "rhoana.*", ens, frame, ".Rdata", sep=""))
+summarise.rho <- function(ens, frame, irrep, PC, threshold=-0.1, shift=pi) {
+  filelist <- Sys.glob(paste("./", "rhoana.", PC, ".*", ens, ".", frame, ".", irrep, ".Rdata", sep=""))
   
   N <- length(filelist)
-  cat("processing", N, "data files for ensemble", ens, "\n")
+  cat("processing", N, "data files for ensemble", ens, "and", PC, "\n")
   load(filelist[1])
   R <- pc.matrixfit$boot.R
   rr <- c(2:(R+1))
   
-  res <- array(0, dim=c(R+1, N, 6))
+  res <- array(0, dim=c(R+1, N, 4))
   
   for(i in c(1:N)) {
     load(filelist[i])
     
-    ## ECM1
+    ## ECM
     res[1,i,1] <- gs$Ecm
     res[rr,i,1] <- gs$Ecmboot
-    ## ECM2
-    res[1,i,2] <- fes$Ecm
-    res[rr,i,2] <- fes$Ecmboot
     
-    ##delta1
-    res[1,i,3] <- gs$delta
-    res[rr,i,3] <- gs$deltaboot
-    ##delta1
-    res[1,i,4] <- fes$delta
-    res[rr,i,4] <- fes$deltaboot
-    ## Qval1
-    res[1,i,5] <- pc.matrixfit$Qval
-    ## Qval2
-    res[1,i,6] <- pc2.matrixfit$Qval
+    ##delta
+    res[1,i,2] <- gs$delta
+    res[rr,i,2] <- gs$deltaboot
+
+    ## shift where neccessary
+    ii <- which(res[,i,2] < threshold)
+    res[ii,i,2] <- res[ii,i,2]+shift
+
+    ##tan(delta)
+    res[1,i,3] <- gs$tandelta
+    res[rr,i,3] <- gs$tandeltaboot
+
+    ## Qvals
+    res[1,i,4] <- pc.matrixfit$Qval
+    res[2,i,4] <- pion.matrixfit$Qval
   }
 
-  save(res, file=paste(path, "res.", ens, frame, ".Rdata", sep=""))
+  save(res, file=paste("res", ens, frame, irrep, "Rdata", sep="."))
   return(invisible(res))
 }
 
-##source("../../pipi/analyse_pipi.R")
-
 estimate.error <- function(res, index=1, prob=c(0.1573, 0.8427), main) {
-  j <- 5
-  if(index %% 2 == 0) j <- 6
-  pvalues <- as.vector((1-2*abs(res[1,,j]-0.5)))
+  pvalues <- as.vector((1-2*abs(res[1,,4]-0.5)))
   
   err <- apply(res[,,index], 2, sd, na.rm=TRUE)
   w <- compute.weights(err, pvalues)
@@ -70,11 +66,16 @@ estimate.error <- function(res, index=1, prob=c(0.1573, 0.8427), main) {
   return(x)
 }
 
-compute.error.rho <- function(res, prob=c(0.1573, 0.8427)) {
-  Ecm1 <- estimate.error(res, index=1, prob=prob, main="Ecm1")
-  Ecm2 <- estimate.error(res, index=2, prob=prob, main="Ecm2")
-  delta1 <- estimate.error(res, index=3, prob=prob, main="delta1")
-  delta2 <- estimate.error(res, index=4, prob=prob, main="delta2")
+compute.error.rho <- function(res, prob=c(0.1573, 0.8427), PC) {
+  Ecm <- estimate.error(res, index=1, prob=prob, main="Ecm")
+  delta <- estimate.error(res, index=2, prob=prob, main="delta")
+  tandelta <- estimate.error(res, index=3, prob=prob, main="tandelta")
   
-  return(list(Ecm1=Ecm1, Ecm2=Ecm2, delta1=delta1, delta2=delta2))
+  return(list(Ecm=Ecm, delta=delta, tandelta=tandelta, PC=PC))
+}
+
+shift.delta <- function(res, threshold=-0.1, shift=pi) {
+  ii <- which(res[,,2] < threshold)
+  res[ii] <- res[ii] + shift
+  return(res)
 }
