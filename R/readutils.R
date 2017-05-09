4ea955aeacfdba94d2de1018fc33b20a144cc9a1
Carsten Urbach <curbach@gmx.de>
2017-05-09 19:42:34 +0200

    allow readtextcf to be used without averaging +-t

--- R/readutils.R
+++ R/readutils.R
@@ -212,7 +212,7 @@ readoutputdata <- function(filename) {
   return(invisible(data))
 }
 
-readtextcf <- function(file, T=48, sym=TRUE, path="", skip=1, check.t=0, ind.vector=c(2,3)) {
+readtextcf <- function(file, T=48, sym=TRUE, path="", skip=1, check.t=0, ind.vector=c(2,3), average=TRUE) {
   if(missing(file)) {
     stop("files must be given! Aborting...\n")
   }
@@ -237,7 +237,9 @@ readtextcf <- function(file, T=48, sym=TRUE, path="", skip=1, check.t=0, ind.vec
 
   tmp <- array(tmp[[ind.vector[1]]] + 1i*tmp[[ind.vector[2]]], dim=c(T, length(tmp[[ind.vector[1]]])/T))
   ## average +-t
-  tmp[i1,] <- 0.5*(tmp[i1,] + sign * tmp[i2,])
+  if(average) {
+    tmp[i1,] <- 0.5*(tmp[i1,] + sign * tmp[i2,])
+  }
 
   ret <- list(cf=t(Re(tmp[ii,])), icf=t(Im(tmp[ii,])), Time=T, nrStypes=1, nrObs=1, boot.samples=FALSE, jackknife.samples=FALSE)
   attr(ret, "class") <- c("cf", class(ret))
