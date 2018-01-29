f80ec9cc4d408c2f7516dedead7002b1f5e81a46
Carsten Urbach <curbach@gmx.de>
2018-01-29 18:36:41 +0100

    function for tex formating of numbers with errors

---
+++ R/tex-utils.R
@@ -0,0 +1,13 @@
+tex.catwitherror <- function(x, dx, digits=1) {
+  if(missing(dx)) {
+    cat("$", x, "$", sep="")
+  }
+  else {
+    N <- 0
+    threshold <- 10^(digits-1)
+    while(round(10^N*dx) < threshold) {
+      N <- N+1
+    }
+    cat("$", format(round(x, digits=N), nsmall=N), "(", round(10^N*dx), ")$", sep="")
+  }
+}
