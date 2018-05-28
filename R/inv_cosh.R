4904e4fd1f994dca7a0a57fd46babee3d6b9974e
Johann Ostmeyer <j.ostmeyer@fz-juelich.de>
2018-05-28 17:42:12 +0200

    The function 'invcosh' needed for calculations of the effective mass using 'solve' for a cosh-functionhas been rewritten in C. This leads to significant speedup.

--- R/inv_cosh.R
+++ R/inv_cosh.R
@@ -6,25 +6,6 @@ invcosh <- function(ratio, timeextent, t, eps=1.e-9, maxiterations=1000) {
     return(NA);
 #    stop("Error: ratio is smaller than 1 in invcosh!")
   }
-  mass <- log(ratio)
-  newmass <- log(ratio)
-  i <-  0
-  repeat {
-    mass = newmass
-    i=i+1
-    if(t <= timeextent/2) {
-      r <-  (1+exp(-mass*(timeextent - 2*(t-1) - 2.)))/(1+exp(-mass*(timeextent - 2.*(t-1))))
-    }
-    else {
-      r <-  (1+exp(-mass*(timeextent - 2*(t-1) + 2.)))/(1+exp(-mass*(timeextent - 2.*(t-1))))
-    }
-    newmass = log(ratio*r)
-#    cat("iteration = ",i, maxiterations,"\n")
-#    cat("mass = ",mass, "newmass = ", newmass, "\n")
-    if((abs(mass-newmass) < eps) || (i > maxiterations)) {
-      return(newmass)
-    }
-  }
 
-  return(mass)
+  return(.Call("invcosh", ratio, timeextent, t, eps, maxiterations))
 }
