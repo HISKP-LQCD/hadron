b4ec55ca93802631b52d254cf64846b39d06b3f9
Martin Ueding <dev@martin-ueding.de>
2018-11-20 18:53:54 +0100

    Move initial guess into the model

--- R/new_matrixfit.R
+++ R/new_matrixfit.R
@@ -1,84 +1,113 @@
 library(R6)
 
-Model <- R6Class(
-    'Model',
-    list(
-        prediction = function (par, t) {
-            stop('This is an abstract function.')
-        },
-        prediction_jacobian = function (par, t) {
-            stop('This is an abstract function.')
+MatrixModel <- R6Class(
+  'MatrixModel',
+  public = list(
+    initialize = function (time_extent, parind, sign_vec, ov_sign_vec) {
+      self$time_extent <- time_extent
+      self$parind <- parind
+      self$sign_vec <- sign_vec
+      self$ov_sign_vec <- ov_sign_vec
+    },
+    prediction = function (par, t) {
+      stop('This is an abstract function.')
+    },
+    prediction_jacobian = function (par, t) {
+      stop('This is an abstract function.')
+    },
+    initial_guess = function (corr) {
+      par <- numeric(max(self$parind))
+      j <- which(parlist[1, ] == 1 & parlist[2, ] == 1)
+      par[1] <- invcosh(corr[t1p1 + (j-1) * Thalfp1] / CF$Cor[t1p1 + (j-1) * Thalfp1 + 1], t = t1p1, cf$T)
+      ## catch failure of invcosh
+      if(is.na(par[1]) || is.nan(par[1]))
+        par[1] <- 0.2
+      ## the amplitudes we estimate from diagonal elements
+      for (i in 2:length(par)) {
+        j <- which(parlist[1, ] == (i-1) & parlist[2, ] == (i-1))
+        if (length(j) == 0) {
+          ##if(full.matrix) warning("one diagonal element does not appear in parlist\n")
+          j <- i-1
         }
-    )
+        par[i] <- sqrt(abs(corr[t1p1 + (j-1) * Thalfp1]) / 0.5 / exp(-par[1] * t1))
+      }
+
+      return (par)
+    }
+  )
 )
 
 SingleModel <- R6Class(
-    'SingleModel',
-    inherit = Model,
-    public = list(
-        initialize = function (time_extent, parind, sign_vec, ov_sign_vec) {
-            self$time_extent <- time_extent
-            self$parind <- parind
-            self$sign_vec <- sign_vec
-            self$ov_sign_vec <- ov_sign_vec
-        },
-        prediction = function (par, t) {
-            self$ov_sign_vec * 0.5 * par[self$parind[, 1]] * par[self$parind[, 2]] *
-                (exp(-par[1] * t) + self$sign_vec * exp(-par[1] * (self$time_extent - t)))
-        },
-        prediction_gradient = function (par, t) {
-            ## Derivative with respect to the mass, `par[1]`.
-            zp <- self$ov_sign_vec * 0.5 * par[parind[, 1]] * par[parind[, 2]] *
-                (-t * exp(-par[1] * t) -
-                     (self$time_extent-t) * self$sign_vec * exp(-par[1] * (self$time_extent-t)))
-            res <- zp
+  'SingleModel',
+  inherit = MatrixModel,
+  public = list(
+    prediction = function (par, t) {
+      self$ov_sign_vec * 0.5 * par[self$parind[, 1]] * par[self$parind[, 2]] *
+        (exp(-par[1] * t) + self$sign_vec * exp(-par[1] * (self$time_extent - t)))
+    },
+    prediction_gradient = function (par, t) {
+      ## Derivative with respect to the mass, `par[1]`.
+      zp <- self$ov_sign_vec * 0.5 * par[parind[, 1]] * par[parind[, 2]] *
+        (-t * exp(-par[1] * t) -
+           (self$time_extent-t) * self$sign_vec * exp(-par[1] * (self$time_extent-t)))
+      res <- zp
+      
+      ## Derivatives with respect to the amplitudes.
+      for (i in 2:length(par)) {
+        zp1 <- rep(0, length(zp))
+        j <- which(parind[, 1] == i)
+        zp1[j] <- -self$ov_sign_vec * 0.5 * par[parind[j, 2]] *
+          (exp(-par[1] * t[j]) + self$sign_vec[j] * exp(-par[1] * (self$time_extent-t[j])))
+        
+        zp2 <- rep(0, length(zp))
+        j <- which(parind[, 2] == i)
+        zp2[j] <- -self$ov_sign_vec * 0.5 * par[parind[j, 1]] *
+          (exp(-par[1] * t[j]) + self$sign_vec[j] * exp(-par[1] * (self$time_extent-t[j])))
+        
+        res <- c(res, zp1 + zp2)
+      }
+      
+      return (res)
+    }
+  )
+)
 
-            ## Derivatives with respect to the amplitudes.
-            for (i in 2:length(par)) {
-                zp1 <- rep(0, length(zp))
-                j <- which(parind[, 1] == i)
-                zp1[j] <- -self$ov_sign_vec * 0.5 * par[parind[j, 2]] *
-                    (exp(-par[1] * t[j]) + self$sign_vec[j] * exp(-par[1] * (self$time_extent-t[j])))
+TwoStateModel <- R6Class(
+  'TwoStateModel',
+  inherit = MatrixModel,
+  public = list(
+    initialize = function (time_extent, parind, sign_vec, ov_sign_vec, reference_time) {
+      super$initialize(time_extent, parind, sign_vec, ov_sign_vec)
+      self$reference_time <- reference_time
+    },
+    initial_guess = function (corr) {
+      par = numeric(3)
 
-                zp2 <- rep(0, length(zp))
-                j <- which(parind[, 2] == i)
-                zp2[j] <- -self$ov_sign_vec * 0.5 * par[parind[j, 1]] *
-                    (exp(-par[1] * t[j]) + self$sign_vec[j] * exp(-par[1] * (self$time_extent-t[j])))
+      ## the ground state energy
+      par[1] <- log(corr[reference_time+1]/corr[reference_time+2])
+      ## the deltaE
+      par[2] <- log(corr[reference_time+1]/corr[reference_time+2]) - par[1]
+      par[2] <- 1.
+      ## the amplitude
+      par[3] <- 1.
 
-                res <- c(res, zp1 + zp2)
-            }
-
-            return (res)
-        }
-    ),
-    private = list(
-        time_extent = NA,
-        parind = NA,
-        sign_vec = NA,
-        ov_sign_vec = NA
-    )
+      return (par)
+    }
+  )
 )
 
 Phi4Model <- R6Class(
-    'Phi4Model',
-    inherit = Model,
-    public = list(
-        initialize = function (time_extent, sign_vec, ov_sign_vec, n_particle) {
-            self$time_extent <- time_extent
-            self$sign_vec <- sign_vec
-            self$ov_sign_vec <- ov_sign_vec
-            self$n_particle <- n_particle
-        },
-        prediction = function (par, t) {
-            return (10 + self$n_particle)
-        }
-    ),
-    private = list(
-        time_extent = NA,
-        sign_vec = NA,
-        ov_sign_vec = NA,
-        n_particle = NA
-    )
+  'Phi4Model',
+  inherit = MatrixModel,
+  public = list(
+    initialize = function (time_extent, parind, sign_vec, ov_sign_vec, n_particle) {
+      super$initialize(time_extent, parind, sign_vec, ov_sign_vec)
+      self$n_particle <- n_particle
+    },
+    prediction = function (par, t) {
+      return (10 + self$n_particle)
+    }
+  ),
 )
 
 new_matrixfit <- function(cf,
@@ -94,30 +123,30 @@ new_matrixfit <- function(cf,
                           every) {
   stopifnot(inherits(cf, 'cf_meta'))
   stopifnot(inherits(cf, 'cf_boot'))
-
+  
   if(model == 'pc') {
     stopifnot(inherits(cf, 'cf_principal_correlator'))
   }
-
+  
   stopifnot(cf$symmetrised == TRUE)
-
+  
   t1p1 <- t1 + 1
   t2p1 <- t2 + 1
-
+  
   N <- dim(cf$cf)[1]
   Thalfp1 <- cf$Time/2 + 1
   t <- c(0:(cf$Time/2))
-
+  
   ## This is the number of correlators in cf
   if(!is.null(dim(cf$cf)))
     mSize <- dim(cf$cf)[2] / Thalfp1
   else
     mSize <- dim(cf$cf.tsboot$t)[2] / Thalfp1
-
+  
   if (model == 'pc' && mSize != 1) {
     stop('For model pc only a 1x1 matrix is allowed.')
   }
-
+  
   if (missing(parlist)) {
     if (mSize == 1) {
       parlist <- array(c(1, 1), dim = c(2, 1))
@@ -131,7 +160,7 @@ new_matrixfit <- function(cf,
       stop("parlist is missing and no default is available for this cf size!")
     }
   }
-
+  
   if (missing(sym.vec)) {
     if (mSize == 1) {
       sym.vec <- c("cosh")
@@ -145,7 +174,7 @@ new_matrixfit <- function(cf,
       stop("sym.vec is missing and no default is available for this cf size!")
     }
   }
-
+  
   if (missing(neg.vec)) {
     if (mSize == 1) {
       neg.vec <- c(1)
@@ -159,7 +188,7 @@ new_matrixfit <- function(cf,
       stop("neg.vec is missing and no default is available for this cf size!")
     }
   }
-
+  
   ## some sanity checks
   if (min(parlist) <= 0) {
     stop("Elements of parlist must be all > 0!")
@@ -169,7 +198,7 @@ new_matrixfit <- function(cf,
       stop("not all parameters are used in the fit!")
     }
   }
-
+  
   if (dim(parlist)[2] != mSize) {
     cat(mSize, dim(parlist)[2], "\n")
     stop("parlist has not the correct length! Aborting! Use e.g. extractSingleCor.cf or c to bring cf to correct number of observables\n")
@@ -180,9 +209,9 @@ new_matrixfit <- function(cf,
   if (length(neg.vec) != mSize){
     stop("neg.vec does not have the correct length! Aborting\n")
   }
-
+  
   CF <- data.frame(t = t, Cor = cf$cf0, Err = apply(cf$cf.tsboot$t, 2, cf$error_fn))
-
+  
   ## index vector for timeslices to be fitted
   ii <- c((t1p1):(t2p1))
   if (mSize > 1) {
@@ -198,7 +227,7 @@ new_matrixfit <- function(cf,
   if (!missing(every)) {
     ii <- ii[ii %% every == 0]
   }
-
+  
   ## parind is the index vector for the matrix elements
   ## signvec decides on cosh or sinh
   ## ov.sign.vec indicates the overall sign
@@ -208,52 +237,25 @@ new_matrixfit <- function(cf,
   for (i in 1:mSize) {
     parind[((i-1)*Thalfp1+1):(i*Thalfp1),] <- t(array(parlist[,i]+1, dim=c(2,Thalfp1)))
     if (sym.vec[i] == "sinh")
-        sign.vec[((i-1)*Thalfp1+1):(i*Thalfp1)] <- -1
+      sign.vec[((i-1)*Thalfp1+1):(i*Thalfp1)] <- -1
     if (sym.vec[i] == "exp")
-        sign.vec[((i-1)*Thalfp1+1):(i*Thalfp1)] <- 0
+      sign.vec[((i-1)*Thalfp1+1):(i*Thalfp1)] <- 0
     if (neg.vec[i] == -1)
-        ov.sign.vec[((i-1)*Thalfp1+1):(i*Thalfp1)] <- -1
+      ov.sign.vec[((i-1)*Thalfp1+1):(i*Thalfp1)] <- -1
   }
-
+  
   if (model == 'single') {
-      model_object <- SingleModel$new(cf$Time, parind, sign.vec, ov.sign.vec)
+    model_object <- SingleModel$new(cf$Time, parind, sign.vec, ov.sign.vec)
   } else if (model == 'shifted') {
-      cf$deltat
+    cf$deltat
   } else if (model == 'pc') {
-      cf$gevp_reference_time
+    cf$gevp_reference_time
   } else if (model == 'n_particles') {
-      cf$n_particles
+    cf$n_particles
   }
 
-  par <- numeric(max(parind))
-  ## we get initial guesses for fit parameters from effective masses
-  ## first is the mass
-  ## (we currently allow for only one)
-  if (model == 'pc') {
-    ## the ground state energy
-    par[1] <- log(CF$Cor[reference_time+1]/CF$Cor[reference_time+2])
-    ## the deltaE
-    par[2] <- log(CF$Cor[reference_time+1]/CF$Cor[reference_time+2]) - par[1]
-    par[2] <- 1.
-    ## the amplitude
-    par[3] <- 1.
-  }
-  else {
-    j <- which(parlist[1,]==1 & parlist[2,]==1)
-    par[1] <- invcosh(CF$Cor[t1p1+(j-1)*Thalfp1]/CF$Cor[t1p1+(j-1)*Thalfp1+1], t=t1p1, cf$T)
-    ## catch failure of invcosh
-    if(is.na(par[1]) || is.nan(par[1])) par[1] <- 0.2
-    ## the amplitudes we estimate from diagonal elements
-    for(i in 2:length(par)) {
-      j <- which(parlist[1,]==(i-1) & parlist[2,]==(i-1))
-      if(length(j) == 0) {
-        ##if(full.matrix) warning("one diagonal element does not appear in parlist\n")
-        j <- i-1
-      }
-      par[i] <- sqrt(abs(CF$Cor[t1p1+(j-1)*Thalfp1])/0.5/exp(-par[1]*t1))
-    }
-  }
-
+  par <- model_object$initial_guess(CF$Cor)
+  
   args <- list(fn = model_object$prediction,
                par.guess = par,
                y = CF$Cor[ii],
@@ -262,10 +264,10 @@ new_matrixfit <- function(cf,
                use.minpack.lm = fit.method == 'lm',
                error = cf$error_fn,
                cov_fn = cf$cov_fn)
-
+  
   if (useCov) {
     args$CovMatrix <- cf$cov_fn(cf$cf.tsboot$t[, ii])
   }
-
+  
   do.call(bootstrap.nlsfit, args)
 }
