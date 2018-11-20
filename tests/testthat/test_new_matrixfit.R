a0886344a1fd79c90737233d64a3fb0851fd0edf
Martin Ueding <dev@martin-ueding.de>
2018-11-20 20:16:07 +0100

    Add more failing tests

--- tests/testthat/test_new_matrixfit.R
+++ tests/testthat/test_new_matrixfit.R
@@ -1,17 +1,53 @@
 context('new_matrixfit')
 
-samplecf_boot <- bootstrap.cf(samplecf, 100)
 
 test_that('SimpleModel', {
-  ## We perform the fit using the old `matrixfit` function.
-  fit_old <- matrixfit(samplecf_boot, 10, 15, fit.method = 'lm')
+  samplecf_boot <- bootstrap.cf(samplecf, 100)
   
+  fit_old <- matrixfit(samplecf_boot, 10, 15, fit.method = 'lm')
   fit_new <- new_matrixfit(samplecf_boot, 10, 15, fit.method = 'lm')
   
   expect_equal(fit_old$t0, fit_new$t0, tolerance = 1e-4)
+  
+  ## The `matrixfit` function gives the chi² as part of the bootstrap samples,
+  ## we do not want this here. Therefore we need to cut.
   expect_equal(fit_old$t[, 1:2], fit_new$t, tolerance = 1e-4)
   expect_equal(fit_old$dof, fit_new$dof)
   expect_equal(fit_old$chisqr, fit_new$chisqr)
-  expect_equal(fit_old$Qval, fit_new$Qval, tolerance = 1e-4)
+  expect_equal(fit_old$Qval, fit_new$Qval)
+})
+
+test_that('ShiftedModel', {
+  samplecf_shited <- hadron::shift.cf(samplecf, 1)
+  samplecf_boot <- bootstrap.cf(samplecf_shited, 100)
   
+  fit_old <- matrixfit(samplecf_boot, 10, 15, model = 'shifted', fit.method = 'lm')
+  fit_new <- new_matrixfit(samplecf_boot, 10, 15, model = 'shifted', fit.method = 'lm')
+  
+  expect_equal(fit_old$t0, fit_new$t0, tolerance = 1e-4)
+  
+  ## The `matrixfit` function gives the chi² as part of the bootstrap samples,
+  ## we do not want this here. Therefore we need to cut.
+  expect_equal(fit_old$t[, 1:2], fit_new$t, tolerance = 1e-4)
+  expect_equal(fit_old$dof, fit_new$dof)
+  expect_equal(fit_old$chisqr, fit_new$chisqr)
+  expect_equal(fit_old$Qval, fit_new$Qval)
+})
+
+test_that('TwoStateModel', {
+  samplecf_boot <- bootstrap.cf(samplecf, 100)
+  gevp <- bootstrap.gevp(samplecf_boot, t0 = 13, element.order = 1:4)
+  pc <- gevp2cf(gevp, 1)
+  
+  fit_old <- matrixfit(pc, 10, 15, model = 'pc', fit.method = 'lm')
+  fit_new <- new_matrixfit(pc, 10, 15, model = 'pc', fit.method = 'lm')
+  
+  expect_equal(fit_old$t0, fit_new$t0, tolerance = 1e-4)
+  
+  ## The `matrixfit` function gives the chi² as part of the bootstrap samples,
+  ## we do not want this here. Therefore we need to cut.
+  expect_equal(fit_old$t[, 1:2], fit_new$t, tolerance = 1e-4)
+  expect_equal(fit_old$dof, fit_new$dof)
+  expect_equal(fit_old$chisqr, fit_new$chisqr)
+  expect_equal(fit_old$Qval, fit_new$Qval)
 })
