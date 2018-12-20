cae80620740828008c9fcf60035678d5cf39e17a
Martin Ueding <dev@martin-ueding.de>
2018-12-20 15:43:18 +0100

    More boot::

--- R/onlinemeas.R
+++ R/onlinemeas.R
@@ -151,13 +151,13 @@ onlinemeas <- function(data, t1, t2,
                       fit.routine=fit.routine)
   }
   if(method == "boot" || method == "all") {
-    fit.boot <- boot(data=t(W[ii,]), statistic=fit.online.boot, R=boot.R, stype="i",
-                     Time=Time, t1=t1, t2=t2, Err=E[ii], par=par2,
-                     fit.routine=fit.routine)
+    fit.boot <- boot::boot(data=t(W[ii,]), statistic=fit.online.boot, R=boot.R, stype="i",
+                           Time=Time, t1=t1, t2=t2, Err=E[ii], par=par2,
+                           fit.routine=fit.routine)
 
-    fit.tsboot <- tsboot(tseries=t(W[ii,]), statistic=fit.online.boot, R=boot.R, l=boot.l, sim=tsboot.sim,
-                         Time=Time, t1=t1, t2=t2, Err=E[ii], par=par2, 
-                         fit.routine=fit.routine)
+    fit.tsboot <- boot::tsboot(tseries=t(W[ii,]), statistic=fit.online.boot, R=boot.R, l=boot.l, sim=tsboot.sim,
+                               Time=Time, t1=t1, t2=t2, Err=E[ii], par=par2, 
+                               fit.routine=fit.routine)
   }
 
   Chi <- rep(0., times=2*T1)
