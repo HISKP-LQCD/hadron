ece78d72f7e763594f4010f0dec5ff50bba8f1cc
Martin Ueding <dev@martin-ueding.de>
2019-02-03 20:35:44 +0100

    Create new_X11_if_appropriate

--- R/plotutils.R
+++ R/plotutils.R
@@ -476,3 +476,12 @@ drawXbars <- function(x,y,dx,length=0.01,...) {
 #box()
 #dev.off()
 #par(opar)
+
+new_X11_if_appropriate <- function () {
+  is_X11 <- grepl(pattern="X11", x=names(dev.cur()), ignore.case=TRUE)
+  is_null <- grepl(pattern="null", x=names(dev.cur()), ignore.case=TRUE)
+
+  if (interactive() && (is_X11 || is_null)) {
+    X11()
+  }
+}
