145a04a29b4610d426ca5103e1f4c1bc70b2d0c4
Martin Ueding <dev@martin-ueding.de>
2019-08-05 10:01:57 +0200

    Reflect the rename also in the unit tests

--- tests/testthat/test_new_matrixfit.R
+++ tests/testthat/test_new_matrixfit.R
@@ -1,4 +1,4 @@
-context('new_matrixfit')
+context('matrixfit')
 
 test_that('SingleModelJacobian', {
   time_extent <- 10
@@ -25,13 +25,13 @@ test_that('SingleModel', {
   samplecf_boot <- bootstrap.cf(samplecf, 100)
   
   args <- list(samplecf_boot, 5, 10, fit.method = 'lm', model = 'single')
-  fit_old <- do.call(matrixfit, args)
-  fit_new <- do.call(new_matrixfit, args)
+  fit_old <- do.call(old_matrixfit, args)
+  fit_new <- do.call(matrixfit, args)
   
   expect_equal(fit_old$t0, fit_new$t0, tolerance = 1e-4)
   
-  ## The `matrixfit` function gives the chi² as part of the bootstrap samples,
-  ## we do not want this here. Therefore we need to cut.
+  ## The `old_matrixfit` function gives the chi² as part of the bootstrap
+  ## samples, we do not want this here. Therefore we need to cut.
   expect_equal(fit_old$t[, 1:2], fit_new$t, tolerance = 1e-4)
   expect_equal(fit_old$dof, fit_new$dof)
   expect_equal(fit_old$chisqr, fit_new$chisqr)
@@ -65,13 +65,13 @@ test_that('ShiftedModel', {
   samplecf_boot <- bootstrap.cf(samplecf_shited, 100)
   
   args <- list(samplecf_boot, 5, 10, model = 'shifted', fit.method = 'lm', sym.vec = 'sinh')
-  fit_old <- do.call(matrixfit, args)
-  fit_new <- do.call(new_matrixfit, args)
+  fit_old <- do.call(old_matrixfit, args)
+  fit_new <- do.call(matrixfit, args)
   
   expect_equal(fit_old$t0, fit_new$t0, tolerance = 1e-4)
   
-  ## The `matrixfit` function gives the chi² as part of the bootstrap samples,
-  ## we do not want this here. Therefore we need to cut.
+  ## The `old_matrixfit` function gives the chi² as part of the bootstrap
+  ## samples, we do not want this here. Therefore we need to cut.
   expect_equal(fit_old$t[, 1:2], fit_new$t, tolerance = 1e-4)
   expect_equal(fit_old$dof, fit_new$dof)
   expect_equal(fit_old$chisqr, fit_new$chisqr)
@@ -84,16 +84,16 @@ test_that('TwoStateModel', {
   pc <- gevp2cf(gevp, 1)
   
   args <- list(pc, 2, 15, model = 'pc', fit.method = 'lm')
-  fit_old <- do.call(matrixfit, args)
-  fit_new <- do.call(new_matrixfit, args)
+  fit_old <- do.call(old_matrixfit, args)
+  fit_new <- do.call(matrixfit, args)
   
   expect_equal(fit_old$t0, fit_new$t0, tolerance = 1e-6)
   
   expect_gte(fit_new$t0[1], 0)
   expect_gte(fit_new$t0[2], 0)
   
-  ## The `matrixfit` function gives the chi² as part of the bootstrap samples,
-  ## we do not want this here. Therefore we need to cut.
+  ## The `old_matrixfit` function gives the chi² as part of the bootstrap
+  ## samples, we do not want this here. Therefore we need to cut.
   expect_equal(fit_old$t[, 1:3], fit_new$t)
   expect_equal(fit_old$dof, fit_new$dof)
   expect_equal(fit_old$chisqr, fit_new$chisqr)
