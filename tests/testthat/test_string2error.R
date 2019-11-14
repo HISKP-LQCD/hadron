9950e64c1b1f934fd5bd8990e2cb05d6472ea2d7
Martin Ueding <dev@martin-ueding.de>
2019-11-14 11:29:44 +0100

    Add some more mean tests

--- tests/testthat/test_string2error.R
+++ tests/testthat/test_string2error.R
@@ -13,4 +13,9 @@ test_that('1', {
   expect_equal(string2error('0.001(200)'), c(0.001, 0.200))
   expect_equal(string2error('0.001(2000)'), c(0.001, 2.000))
   expect_equal(string2error('0.001(20000)'), c(0.001, 20.000))
+  
+  expect_equal(string2error('.001(2)'), c(0.001, 0.002))
+  expect_equal(string2error('-.001(2)'), c(-0.001, 0.002))
+  expect_equal(string2error('310.(2)'), c(310, 2))
+  expect_equal(string2error('-310.(2)'), c(-310, 2))
 })
