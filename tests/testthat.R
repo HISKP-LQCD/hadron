caf6905386c46d6cf20edfb32305673e9eb9237a
Martin Ueding <mu@martin-ueding.de>
2020-05-04 11:36:25 +0200

    Only run the unit tests on Linux

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,4 +1,10 @@
 library(testthat)
 library(hadron)
 
-test_check("hadron")
+# The developers of this package only use Linux or the Linux subsystem on
+# Windows. Therefore we do not want to invest time in getting all the unit
+# tests working on Windows. If the operating system is not linux, we just don't
+# run our unit tests.
+if (Sys.info()['sysname'] == 'Linux') {
+  test_check("hadron")
+}
