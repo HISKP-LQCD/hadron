3f90f0f926df43ce75749cc0e99a3e809c9bd0af
Carsten Urbach <curbach@gmx.de>
2020-05-25 12:58:36 +0200

    removed *.cf again, should be added elsewhere

--- R/cf.R
+++ R/cf.R
@@ -941,55 +941,6 @@ add.cf <- function(cf1, cf2, a = 1.0, b = 1.0) {
   return (cf)
 }
 
-#' Multiply two cf objects by each other measurement by measurement
-#'
-#' Note that no complex arithmetic is used, real and imaginary parts are 
-#' treated as seperate and indepenent, such that the real part of one
-#' is the divided by the real part of the other and similarly for the
-#' imaginary parts.
-#'
-#' Note that this is generally only allowed on bootstrap samples and mean values,
-#' although it makes sense in some exeptional circumstances. Don't use this
-#' function unless you're certain that you should!
-#' 
-#' @param cf1,cf2 `cf_orig` objects.
-#'
-#' @return
-#' The value is
-#' \deqn{cf1 * cf2 \,.}
-#' @export
-'*.cf' <- function(cf1, cf2) {
-  stopifnot(inherits(cf1, 'cf_meta'))
-  stopifnot(inherits(cf2, 'cf_meta'))
-  cf <- cf1
-  if(inherits(cf1, 'cf_orig') && inherits(cf2, 'cf_orig')) {
-    stopifnot(all(dim(cf1$cf) == dim(cf2$cf)))
-    stopifnot(cf1$Time == cf2$Time)
-
-    cf$cf <- cf1$cf * cf2$cf
-
-    if( has_icf(cf1) | has_icf(cf2) ){
-      stopifnot(has_icf(cf1) & has_icf(cf2))
-      cf$icf <- cf1$icf * cf2$icf
-    }
-  }
-  ## the following is a bit dangerous, however, for
-  ## principal correlators this is the only way to
-  ## build a ratio
-  if(inherits(cf1, 'cf_boot') && inherits(cf2, 'cf_boot') &&
-     all(dim(cf1$cf.tsboot$t) == dim(cf2$cf.tsboot$t)) &&
-     cf1$seed == cf2$seed && cf1$boot.l == cf2$boot.l) {
-    
-    cf$cf.tsboot$t  <- cf1$cf.tsboot$t * cf2$cf.tsboot$t
-    cf$cf.tsboot$t0 <- cf1$cf.tsboot$t0 * cf2$cf.tsboot$t0
-    cf$tsboot.se <- apply(cf$cf.tsboot$t, MARGIN = 2L, FUN = cf$error_fn)
-    cf$cf0 <- cf$cf.tsboot$t0
-  }
-  else cf <- invalidate.samples.cf(cf)
-  return (cf)
-}
-
-
 #' Arithmetically scale a correlator by a scalar a
 #'
 #' Note that this function is fundamentally different from \code{*.cf}.
